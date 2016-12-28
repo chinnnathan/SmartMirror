fc7b4765db094735585d8aea7ada31513bce31fe
chinnnathan <chinnnathan@gmail.com>
2016-12-28 13:08:14 -0700

    Running Server with OpenWeatherMap API attempt

--- SMRT/SMRT/SMRT/views.py
+++ SMRT/SMRT/SMRT/views.py
@@ -7,16 +7,20 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
+import requests
+import json
 from SMRT import app
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
+    weather = get_weather()
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
+        weather = weather,
     )
 
 @app.route('/contact')
@@ -38,3 +42,25 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+
+def get_weather():
+    """Returns info about the weather"""
+    units = "F"
+    loc = "London"
+    APIKEY = "10612e11aa4786db990fde1fc9c283ad"
+    try:
+        u = "http://api.openweathermap.org/data/2.5/weather?q={0}&units={1}"
+        u = "api.openweathermap.org/data/2.5/weather?q=London&APPID=" + APIKEY
+        r = requests.get(u)
+        # r = requests.get(u.format(loc, units))
+        j = json.loads(r.text)
+    except:
+        print("Fuck You")
+        return "Cannot Get Weather"
+
+    temp = j['main']['temp']
+    temp_unit = 'F' 
+    conditions = j['weather'][0]['description']
+
+    return ( conditions, " with temperature of ", temp, u"\u00b0", " ", temp_unit)
